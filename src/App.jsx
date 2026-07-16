b34276a4b2f60afbebc0638f0a564341fb45842c
Avinesh5009 <avineshkumar448@gmail.com>
2026-07-16 11:52:30 +0530

    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React, { useState } from 'react';
+import './style.css';
 import SplashScreen from "./pages/SplashScreen/SplashScreen";
+import Login from "./pages/Login/Login";
+import HomeScreen1 from "./pages/Home/HomeScreen1";
 
 export default function App() {
-  return <SplashScreen />
+  const [view, setView] = useState('splash');
+
+  return (
+    <div className="app-container">
+      {view === 'splash' && (
+        <SplashScreen onComplete={() => setView('login')} />
+      )}
+      {view === 'login' && (
+        <Login setView={setView} />
+      )}
+      {view === 'home1' && (
+        <HomeScreen1 setView={setView} />
+      )}
+    </div>
+  );
 }
